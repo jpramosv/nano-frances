cdac8c41dd6b5583380a80d098ebca07d6dcc210
João Ramos <jpramos.comp@gmail.com>
2022-09-27 21:15:09 -0300

    added responsively to topoview

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,32 +1,19 @@
 <template>
   <TopoView />
-
-  <IntroducaoView />
-
-  <ApresentacaoView />
-
-  <LaLune />
-
-  <CardsModulos />
 </template>
 
 <script>
 import TopoView from "../components/TopoView.vue";
-import IntroducaoView from "../components/IntroducaoView.vue";
-import ApresentacaoView from "../components/ApresentacaoView.vue";
-import LaLune from "../components/LaLune.vue";
-import CardsModulos from "../components/CardsModulos.vue";
+
 export default {
   name: "HomeView",
   components: {
     TopoView,
-    IntroducaoView,
-    ApresentacaoView,
-    LaLune,
-    CardsModulos,
+
   },
 };
 </script>
 
 <style>
+
 </style>
--- src/components/TopoView.vue
+++ src/components/TopoView.vue
@@ -1,11 +1,14 @@
 <template>
   <section class="home">
-    <h1>Fale francês em três meses</h1>
-    <div>
-      <blockquote class="quote-externo">
-        <p>Mesmo que você não saiba nada ou ache uma língua difícil.</p>
-      </blockquote>
+    <div class="home-container">
+      <h1>Fale francês em três meses</h1>
+      <div>
+        <blockquote class="quote-externo">
+          <p>Com apenas 15 minutos por dia, mesmo que você não saiba nada ou ache uma língua difícil.</p>
+        </blockquote>
+      </div>
     </div>
+
   </section>
 </template>
 
@@ -30,12 +33,34 @@ a {
   margin: 0px;
   padding: 0px;
 }
+
+.home {
+  width: 100%;
+  height: 480px;
+  background: url("@/assets/2.jpg") no-repeat center;
+  background-size: cover;
+  opacity: 90%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.home-container {
+  max-width: 960px;
+
+}
+
 .home h1 {
-  font-size: 3em;
+  font-size: 3rem;
   color: white;
   font-weight: bold;
   text-transform: uppercase;
+  font-family: "Poppins", Sans-serif;
+  font-weight: 700;
+  letter-spacing: 1.1px;
 }
+
 
 .quote-externo {
   max-width: 500px;
@@ -53,39 +78,62 @@ a {
   margin: 10px auto;
 }
 
-.home {
-  text-align: center;
-  width: 100%;
-  height: 380px;
-  background: url("@/assets/2.jpg") no-repeat center;
-  background-size: cover;
-  opacity: 90%;
-}
-
-.home h1 {
+.home p {
   color: #ffffff;
   font-family: "Poppins", Sans-serif;
-  font-size: 3vw;
-  font-weight: 700;
-  letter-spacing: 1.1px;
-  padding-top: 150px;
+  text-align: center;
+  font-size: 1.2rem;
+
+
 }
 
-@media (max-width: 767px) {
+@media (max-width: 1024px) {
   .home {
     text-align: center;
     width: 100%;
-    height: 200px;
+    height: 300px;
     background: url("@/assets/2.jpg") no-repeat center;
-    background-size: contain;
+    background-size: cover;
   }
+
   .home h1 {
     color: #ffffff;
     font-family: "Poppins", Sans-serif;
-    font-size: 3vw;
+    font-size: 2em;
     font-weight: 700;
     letter-spacing: 1.1px;
-    padding-top: 150px;
+
+  }
+
+  .home p {
+
+    font-size: 0.9 em;
+
+  }
+}
+
+@media (max-width: 700px) {
+  .home {
+    text-align: center;
+    width: 100%;
+    height: 300px;
+    background: url("@/assets/3.jpg") no-repeat center;
+    background-size: cover;
+  }
+
+  .home h1 {
+    color: #ffffff;
+    font-family: "Poppins", Sans-serif;
+    font-size: 2em;
+    font-weight: 700;
+    letter-spacing: 1.1px;
+
+  }
+
+  .home p {
+
+    font-size: 0.em;
+
   }
 }
 </style>
